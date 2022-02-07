fdc9199c923633a3e2a544a374b1cd2652643633
AmitFikler <amitfikler@gmail.com>
2022-02-07 12:58:07 +0200

    get all posts from all pages

--- scraping.py
+++ scraping.py
@@ -1,7 +1,7 @@
-from asyncio import constants
-from turtle import pos
 from bs4 import BeautifulSoup
 import requests
+
+# scraping
 
 
 def scraping_date(url):
@@ -14,17 +14,36 @@ def scraping_date(url):
     except:
         print("Error: Cannot connect to the website")
 
+# find content of post
+
+
+def find_content(url):
+    try:
+        soup = scraping_date(url)
+        li = soup.find_all('li')
+        content = ""
+        for i in li:
+            content += " " + i.text
+        return clean_text(content)
+    except:
+        print("Error: Cannot find content")
+
+# convert html to data object
+
 
 def html_to_data(post):
     try:
         dataObj = {
-            "heading": post.find('h4').text.strip('\n').strip('\t'),
+            "heading": clean_text(post.find('h4').text),
             "Author":  post.find('div', {"class": 'col-sm-6'}).text.split(' ')[2],
+            "Content": find_content(post.find('a').get('href')),
             "Date": " ".join(post.find('div', {"class": 'col-sm-6'}).text.split(' ')[4:8])
         }
         return dataObj
     except:
         return
+
+# Find all post of one page
 
 
 def find_all_post(soup):
@@ -32,7 +51,6 @@ def find_all_post(soup):
         data = []
         section = soup.find('section', id='list')
         posts = section.findChildren('div', {"class": 'row'})
-        print(posts[3].find('div', {"class": 'col-sm-6'}).text.split(' ')[2])
         for post in posts:
             if html_to_data(post):
                 data.append(html_to_data(post))
@@ -40,8 +58,30 @@ def find_all_post(soup):
     except:
         print("Error: Cannot find all posts")
 
+# cleaning text from special characters
 
-data = scraping_date(
-    "http://strongerw2ise74v3duebgsvug4mehyhlpa7f6kfwnas7zofs3kov7yd.onion/all")
 
-print(find_all_post(data))
+def clean_text(text):
+    try:
+        return text.replace('\n', '').replace('\t', '').replace('\xa0', '')
+    except:
+        return
+
+
+# get all data from all pages
+def find_all_data_from_pages():
+    all_data = []
+    data = scraping_date(
+        "http://strongerw2ise74v3duebgsvug4mehyhlpa7f6kfwnas7zofs3kov7yd.onion/all")
+    all_data.extend(find_all_post(data))  # add all post of first page
+    pagination = data.find('ul', {"class": 'pagination'})
+    pages = pagination.find_all('li')
+    for page in pages[1:len(pages) - 1]:
+        if page.find('a'):
+            link = page.find('a').get('href')
+            data = scraping_date(link)
+            all_data.extend(find_all_post(data))
+    return all_data
+
+
+print(find_all_data_from_pages())
