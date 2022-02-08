587790d6da8969ee0cf9b1d63db8ad4eae65df79
AmitFikler <amitfikler@gmail.com>
2022-02-08 22:17:48 +0200

    delete unnecessary files

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,62 +1,27 @@
-import React, { useEffect } from 'react';
-import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
-import './App.css';
+import { useEffect, useState } from 'react';
 import axios from 'axios';
+import { Paste } from '../@types/paste';
+import Pastes from './components/pastes';
 
 function App() {
+  const [pastes, setPastes] = useState<Paste[]>([]);
+
   useEffect(() => {
-    const response = axios.get('http://127.0.0.1:8000/data').then((res) => {
-      console.log(res.data);
-    });
+    fetchPaste();
+    setInterval(fetchPaste, 120000);
   }, []);
+
+  const fetchPaste = async () => {
+    const { data } = await axios.get('http://127.0.0.1:8000/data');
+    console.log('FETCHED');
+    setPastes((prevPastes) => [...prevPastes, ...data]);
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Counter />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <span>
-          <span>Learn </span>
-          <a
-            className="App-link"
-            href="https://reactjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux-toolkit.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux Toolkit
-          </a>
-          ,<span> and </span>
-          <a
-            className="App-link"
-            href="https://react-redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React Redux
-          </a>
-        </span>
-      </header>
+      <h1>hello world</h1>
+      {pastes?.map((paste) => (
+        <Pastes paste={paste} />
+      ))}
     </div>
   );
 }
